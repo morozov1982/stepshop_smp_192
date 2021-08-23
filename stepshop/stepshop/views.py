cdb49360c29e2e9ce729457ac0bdd478332c3a75
Александр Морозов <@new.local.itstep>
2021-08-23 17:59:04 +0600

    add: lesson_04

--- stepshop/stepshop/views.py
+++ stepshop/stepshop/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
+from mainapp.models import Product
 
 
 def index(request):
-    return render(request, 'stepshop/index.html')
+    title = 'магазин'
+
+    products = Product.objects.all()  # [:4]
+
+    context = {
+        'title': title,
+        'products': products,
+    }
+
+    return render(request, 'stepshop/index.html', context=context)
 
 
 def contacts(request):
