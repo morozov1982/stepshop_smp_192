94f3c75c387ec9495fb5f849bfb20c14ddfaa585
Александр Морозов <@new.local.itstep>
2021-08-26 17:57:34 +0600

    add: lesson_06

--- stepshop/authapp/views.py
+++ stepshop/authapp/views.py
@@ -2,7 +2,7 @@ from django.contrib import auth
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 
-from authapp.forms import ShopUserLoginForm
+from authapp.forms import ShopUserLoginForm, ShopUserRegisterForm, ShopUserEditForm
 from django.urls import reverse
 
 
@@ -30,3 +30,44 @@ def login(request):
 def logout(request):
     auth.logout(request)
     return HttpResponseRedirect(reverse('index'))
+
+
+def register(request):
+    title = 'регистрация'
+
+    if request.method == "POST":
+        register_form = ShopUserRegisterForm(request.POST, request.FILES)
+
+        if register_form.is_valid():
+            register_form.save()
+            return HttpResponseRedirect(reverse('auth:login'))
+    else:
+        register_form = ShopUserRegisterForm()
+
+    context = {
+        'title': title,
+        'register_form': register_form
+    }
+
+    return render(request, 'authapp/register.html', context)
+
+
+def edit(request):
+    title = 'редактирование'
+
+    if request.method == "POST":
+        edit_form = ShopUserEditForm(request.POST, request.FILES, instance=request.user)
+
+        if edit_form.is_valid():
+            edit_form.save()
+            return HttpResponseRedirect(reverse('auth:edit'))
+    else:
+        edit_form = ShopUserEditForm(instance=request.user)
+
+    context = {
+        'title': title,
+        'edit_form': edit_form
+    }
+
+    return render(request, 'authapp/edit.html', context)
+
--- stepshop/mainapp/views.py
+++ stepshop/mainapp/views.py
@@ -1,19 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+from mainapp.models import ProductCategory, Product
 
 
-def products(request):
+def products(request, pk=0):
     title = 'продукты | каталог'
 
-    filter_menu = [
-        {'filter': '*', 'name': 'All Products'},
-        {'filter': '.new', 'name': 'Newest'},
-        {'filter': '.low', 'name': 'Low Price'},
-        {'filter': '.high', 'name': 'Hight Price'},
-    ]
+    links_menu = ProductCategory.objects.all()
 
     context = {
         'title': title,
-        'filter_menu': filter_menu,
+        'links_menu': links_menu,
     }
+
+    if pk == 0:
+        products = Product.objects.all().order_by('price')
+        category = {'name': 'все'}
+    else:
+        category = get_object_or_404(ProductCategory, pk=pk)
+        products = Product.objects.filter(category__pk=pk).order_by('price')
+
+    context['category'] = category
+    context['products'] = products
 
     return render(request=request, template_name='mainapp/products.html', context=context)
